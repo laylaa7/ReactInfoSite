f2333a8f8ee31b650fabf5cb14dfd40ae2e05459
laylaa7 <148385233+laylaa7@users.noreply.github.com>
2024-08-21 00:29:13 +0300

    main is done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client'
 
 export default function Main(){
     return (
-        <div>
-            <h1>Fun Facts About React</h1>
-            <ul>
+        <main>
+            <h1 className='Main--Heading'> Fun Facts About React</h1>
+            <ul className='Main-Bulletpoints'>
                <li>Was first released in 2013</li>
                <li>Was originally created by Jordan Walke</li>
                <li>Has well over 200K stars on GitHub</li>
                <li>Is maintained by Meta</li>
                <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-            </div>
+            </main>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ export default function NavBar(){
         
             <nav>
             <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
             </a>
             <h3 className='navBar--title'> React Facts</h3>
             <h4 className='navBar--info'> react course - Proj 1</h4>
